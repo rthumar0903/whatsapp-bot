0d662cf60f8e4bfb52d938d40544080512f089a8
raj thumar <thumarraj999@gmail.com>
2024-09-24 22:39:19 +0530

    remove unused code

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/webhook", (req, res) => {
       let phon_no_id =
         body_param.entry[0].changes[0].value.metadata.phone_number_id;
       let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+      // let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
       axios({
         method: "POST",
