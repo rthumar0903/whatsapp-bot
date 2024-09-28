e5759294ec8549cfbec1dbf2fa66c06dbf80c775
raj thumar <thumarraj999@gmail.com>
2024-09-28 15:23:52 +0530

    added try block

--- app/services/meta.services.js
+++ app/services/meta.services.js
@@ -5,32 +5,36 @@ const token = whatsAppConfig.TOKEN;
 const axios = require("axios");
 
 exports.sendLocationMessage = async (phonNoId, userName, phoneNumber) => {
-  axios({
-    method: "POST",
-    url:
-      "https://graph.facebook.com/v13.0/" +
-      phonNoId +
-      "/messages?access_token=" +
-      token,
-    data: {
-      messaging_product: "whatsapp",
-      recipient_type: "individual",
-      type: "interactive",
-      to: phoneNumber,
-      interactive: {
-        type: "location_request_message",
-        body: {
-          text: `Hi ${userName}, Thank you for reaching out. Please help us with your location to help us connect you to our nearest pharmacy partner`,
-        },
-        action: {
-          name: "send_location",
+  try {
+    axios({
+      method: "POST",
+      url:
+        "https://graph.facebook.com/v13.0/" +
+        phonNoId +
+        "/messages?access_token=" +
+        token,
+      data: {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        type: "interactive",
+        to: phoneNumber,
+        interactive: {
+          type: "location_request_message",
+          body: {
+            text: `Hi ${userName}, Thank you for reaching out. Please help us with your location to help us connect you to our nearest pharmacy partner`,
+          },
+          action: {
+            name: "send_location",
+          },
         },
       },
-    },
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 
 exports.sendNotServicableMessage = async (phonNoId, phoneNumber) => {
